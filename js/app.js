fe6fc08268a58e9c3d44b3271dc8ef42d20980ba
Nilxxx11 <80600547+Nilxxx11@users.noreply.github.com>
2024-06-05 10:02:36 -0500

    update js/app.js

--- js/app.js
+++ js/app.js
@@ -18,11 +18,11 @@ let editKey = null;
 const adminPassword = 'n7538';
 let isAdmin = false;
 //la otra seccion
-document.getElementById('seccionButton').addEventListener('click', function() {
-  window.location.href = 'detalles.html';
-});
 document.getElementById('seccionButton1').addEventListener('click', function() {
   window.location.href = 'main.html';
+});
+document.getElementById('seccionButton').addEventListener('click', function() {
+  window.location.href = 'detalles.html';
 });
 document.getElementById('inicio').addEventListener('click', function() {
   window.location.href = 'index.html';
